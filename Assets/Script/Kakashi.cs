cb792603026a3437b94fc22e286a9fe6070a2213
nonochan13 <ueno.jit@gmail.com>
2018-07-27 21:22:46 +0900

    kumagame_small01プロジェクト　初めてのコミット

---
+++ Assets/Script/Kakashi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kakashi : MonoBehaviour {
+    //追憶の画像
+    public Sprite sprite;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+    public void Event()
+    {
+        Debug.Log("tuioku");
+    }
+    public void Touch()
+    {
+        Destroy(this.gameObject);
+    }
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            Debug.Log("kakashi");
+        }
+    }
+}
---
+++ Assets/Script/MenuWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWindow : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI () {
+        // ラベルを表示する
+        GUI.Label(new Rect(10, 10, 100, 100), "ツキノワグマ");
+	}
+
+}
