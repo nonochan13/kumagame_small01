6f775358dd8b0dc83b6f37d74d934df68de5a1dd
nonochan13 <ueno.jit@gmail.com>
2018-08-04 01:09:03 +0900

    kumagame_small01 2回目のコミットです

---
+++ Assets/Script/ColliderUpdate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderUpdate : MonoBehaviour
+{
+    SphereCollider col;
+    private float radius;
+
+    // Use this for initialization
+    void Start()
+    {
+        var kakashi = GetComponent<Kakashi>();
+        radius = kakashi.GetFearRadius(kakashi.fearlevel);
+        var childTransform = this.gameObject.GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in childTransform)
+        {
+            if (null != child.GetComponent<SphereCollider>())
+            {
+                if (child.name == "SphereCollider1")
+                {
+                    child.GetComponent<SphereCollider>().radius = radius - 0.4f;
+                }
+                else if (child.name == "SphereCollider2")
+                {
+                    child.GetComponent<SphereCollider>().radius = radius;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Script/Kakashi.cs
+++ Assets/Script/Kakashi.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Kakashi : MonoBehaviour {
     //追憶の画像
     public Sprite sprite;
+    public Image image;
+    public int fearlevel;
+    
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
     }
     public void Event()
     {
@@ -22,11 +26,42 @@ public class Kakashi : MonoBehaviour {
     {
         Destroy(this.gameObject);
     }
-    public void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        if(other.collider.tag == "Player")
+        //プレイヤーtag のオブジェクトが接触したら
+        if (other.gameObject.tag == "Player")
         {
-            Debug.Log("kakashi");
+            Debug.Log("Kakashi");
+            image.sprite = sprite;
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        //プレイヤーtag のオブジェクトが退去したら
+        Debug.Log("Kakashi");
+        image.sprite = null;
+    }
+    public float GetFearRadius(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                return 5.0f;
+                break;
+            case 2:
+                return 10.0f;
+                break;
+            case 3:
+                return 15.0f;
+                break;
+            case 4:
+                return 20.0f;
+                break;
+            case 5:
+                return 25.0f;
+                break;
+        }
+        return 10.0f;
+    }
+
 }
